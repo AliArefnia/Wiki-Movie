6ce61ccc87e6b10d70de8a90d169c9637c67bd11
AliArefnia <maarefnia1377@gmail.com>
2025-03-13 22:37:28 +0330

    Add small Trailer size card and section

---
+++ components/TrailerSection.vue
@@ -0,0 +1,9 @@
+<template>
+  <div class="flex justify-between items-center px-4 py-2">
+    <p class="text-white">Trailers</p>
+  </div>
+  <div class="flex overflow-auto">
+    <BaseTrailerCard class="shrink-0 mx-2" v-for="n in 6" />
+  </div>
+</template>
+<script setup></script>
---
+++ components/base/BaseTrailerCard.vue
@@ -0,0 +1,11 @@
+<template>
+  <div class="relative rounded-2xl overflow-hidden w-fit font-sans">
+    <div class="absolute left-4 top-4">Trailer</div>
+    <video ref="myVideo" width="200px">
+      <!-- autoplay muted -->
+      <!-- <source src="@/assets/mov_bbb.mp4" type="video/mp4" /> -->
+      <source src="@/assets/mov_bbb.mp4" type="video/mp4" />
+    </video>
+  </div>
+</template>
+<script setup></script>

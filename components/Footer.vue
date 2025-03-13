692d769ccbf17b906e30763027bdc9628e748679
AliArefnia <maarefnia1377@gmail.com>
2025-03-13 22:32:58 +0330

    Add footer and header and tailwind styles
    Also implement nav for mobile view

---
+++ components/Footer.vue
@@ -0,0 +1,10 @@
+<template>
+  <div
+    class="flex justify-around bg-surface-card text-white shadow-lg sticky bottom-0 pb-4"
+  >
+    <BaseButton>1</BaseButton>
+    <BaseButton>2</BaseButton>
+    <BaseButton>3</BaseButton>
+    <BaseButton>4</BaseButton>
+  </div>
+</template>
---
+++ components/HeaderSection.vue
@@ -0,0 +1,100 @@
+<template>
+  <header class="bg-surface-dark text-white shadow-lg">
+    <div>
+      <div class="container mx-auto flex justify-between items-center p-4">
+        <!-- Logo -->
+        <NuxtLink to="/" class="flex items-center space-x-2 text-xl font-bold">
+          <svg
+            class="w-7 h-7 text-primary"
+            fill="none"
+            stroke="currentColor"
+            stroke-width="3"
+            viewBox="0 0 24 24"
+          >
+            <path
+              stroke-linecap="round"
+              stroke-linejoin="round"
+              d="M3 3v18l9-6 9 6V3"
+            ></path>
+          </svg>
+          <span class="">WikiMovie</span>
+        </NuxtLink>
+
+        <!-- Dark Mode Toggle -->
+        <BaseButton @click="toggleNav">
+          <svg
+            class="w-6 h-6 text-primary"
+            fill="none"
+            stroke="currentColor"
+            stroke-width="2"
+            viewBox="0 0 24 24"
+          >
+            <path
+              stroke-linecap="round"
+              stroke-linejoin="round"
+              d="M12 3v1m0 16v1m8-9h1M4 12H3m16.07-7.07l-.71.71M7.05 7.05l-.7-.7M16.95 16.95l.7.7M7.05 16.95l-.7.7M12 5a7 7 0 0 1 0 14 7 7 0 0 1 0-14"
+            ></path>
+          </svg>
+        </BaseButton>
+
+        <transition name="navTransition">
+          <div
+            :class="{ flex: openNavigation, hidden: !openNavigation }"
+            class="flex flex-col w-2/3 md:w-1/3 lg:w-1/4 justify-center items-center h-screen fixed right-0 top-0 bottom-[56px] z-10 bg-surface-hover"
+          >
+            <BaseButton
+              class="absolute top-2 left-2 bg-surface-dark"
+              @click="closeNav"
+              >Close</BaseButton
+            >
+            <p>{{ openNavigation }}</p>
+            <NuxtLink to="">Profile</NuxtLink>
+            <NuxtLink to="">a link</NuxtLink>
+            <NuxtLink to="">a link</NuxtLink>
+            <NuxtLink to="">a link</NuxtLink>
+            <BaseButton>Log Out</BaseButton>
+            <BaseButton>Toggle Dark mode</BaseButton>
+          </div>
+        </transition>
+      </div>
+    </div>
+  </header>
+</template>
+
+<script setup>
+let openNavigation = ref(false);
+
+const toggleNav = () => {
+  openNavigation.value = !openNavigation.value;
+  console.log(openNavigation.value);
+  if (openNavigation.value) {
+    document.body.style.overflow = "hidden";
+  } else {
+    document.body.style.overflow = "auto";
+  }
+};
+
+const closeNav = () => {
+  openNavigation.value = false;
+  document.body.style.overflow = "auto";
+};
+// const openNavi = computed(() => openNavigation);
+</script>
+
+<style scoped>
+.navTransition-enter-to,
+.navTransition-leave-from {
+  opacity: 1;
+  max-width: 400px;
+}
+.navTransition-enter-from,
+.navTransition-leave-to {
+  opacity: 0;
+  max-width: 0;
+}
+
+.navTransition-enter-active,
+.navTransition-leave-active {
+  transition: all 0.3s ease-in;
+}
+</style>

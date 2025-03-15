ce337913c3fa4099352c7baadd3537cb8c76f40a
AliArefnia <maarefnia1377@gmail.com>
2025-03-15 18:33:28 +0330

    add movieImages

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -2,6 +2,13 @@
 import tailwindcss from "@tailwindcss/vite";
 
 export default defineNuxtConfig({
+  runtimeConfig: {
+    TMDB_API_KEY: process.env.TMDB_API_KEY, // Private key (server-side only)
+    public: {
+      TMDB_API_KEY: process.env.NUXT_PUBLIC_TMDB_API_KEY,
+    },
+  },
+
   css: ["@/assets/css/tailwind.css"],
   compatibilityDate: "2024-11-01",
   devtools: { enabled: true },

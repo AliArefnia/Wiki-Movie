25cac1ec5f43a9da64938a3de86992af41473175
AliArefnia <maarefnia1377@gmail.com>
2025-03-19 21:16:25 +0330

    add default layout and use in index

---
+++ layouts/default.vue
@@ -0,0 +1,12 @@
+<template>
+  <HeaderSection />
+  <main class="bg-surface-dark min-h-[100vh]">
+    <slot></slot>
+  </main>
+  <Footer />
+</template>
+
+<script setup>
+import HeaderSection from "~/components/HeaderSection.vue";
+import Footer from "~/components/Footer.vue";
+</script>
--- pages/index.vue
+++ pages/index.vue
@@ -1,29 +1,21 @@
 <template>
-  <HeaderSection />
-  <main class="bg-surface-dark">
-    <Hero></Hero>
-    <TrailerSection />
-    <BaseMovieSection
-      v-for="genre in genres"
-      :genreId="genre.id"
-      :genreName="genre.name"
-    />
-  </main>
-  <Footer />
+  <Hero></Hero>
+  <div class="text-white">{{ x }} , {{ y }}</div>
+  <TrailerSection />
+  <BaseMovieSection
+    v-for="genre in genres"
+    :genreId="genre.id"
+    :genreName="genre.name"
+  />
 </template>
 <script setup>
+definePageMeta({
+  layout: "default",
+});
+
 import { useMovieStore } from "~/store/store";
 
 const movieStore = useMovieStore();
 
 const genres = movieStore.movieGenres;
 </script>
-<!-- 
-genres : Array(19) 0 : {id: 28, name: 'Action'} 1 : {id: 12, name: 'Adventure'}
-2 : {id: 16, name: 'Animation'} 3 : {id: 35, name: 'Comedy'} 4 : {id: 80, name:
-'Crime'} 5 : {id: 99, name: 'Documentary'} 6 : {id: 18, name: 'Drama'} 7 : {id:
-10751, name: 'Family'} 8 : {id: 14, name: 'Fantasy'} 9 : {id: 36, name:
-'History'} 10 : {id: 27, name: 'Horror'} 11 : {id: 10402, name: 'Music'} 12 :
-{id: 9648, name: 'Mystery'} 13 : {id: 10749, name: 'Romance'} 14 : {id: 878,
-name: 'Science Fiction'} 15 : {id: 10770, name: 'TV Movie'} 16 : {id: 53, name:
-'Thriller'} 17 : {id: 10752, name: 'War'} 18 : {id: 37, name: 'Western'} -->

457fc9f9659daf0fd72432ff3ee18c0637bde76f
AliArefnia <maarefnia1377@gmail.com>
2025-03-19 22:53:56 +0330

    Add movie pages

---
+++ pages/[movieName].vue
@@ -0,0 +1,15 @@
+<template>
+  <p>hello</p>
+</template>
+
+<script setup lang="ts">
+definePageMeta({
+  layout: "genre-pages",
+});
+</script>
+
+<style>
+body {
+  font-family: "Inter", sans-serif;
+}
+</style>

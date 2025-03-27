08265f9fbcee7fb78d1b068917381ccedcaf939b
AliArefnia <maarefnia1377@gmail.com>
2025-03-27 22:34:24 +0330

    Footer UI/Logic change
    also fix path issue when the user was in the genre movie, and use the footer to change the page, he will be directed to the wrong page

--- components/Footer.vue
+++ components/Footer.vue
@@ -3,19 +3,53 @@
     class="flex justify-around bg-surface-card text-white shadow-lg sticky bottom-0 py-2 z-20"
   >
     <NuxtLink to="/"
-      ><BaseButton><LayoutGrid /></BaseButton
+      ><BaseButton
+        @click="footerTabSelected('home')"
+        :class="{
+          'bg-surface-hover text-primary': activeFooterTab === 'home',
+          'text-white': !activeFooterTab === 'home',
+        }"
+        ><LayoutGrid /></BaseButton
     ></NuxtLink>
-    <NuxtLink to="search">
-      <BaseButton><Search /></BaseButton>
+    <NuxtLink to="/search">
+      <BaseButton
+        @click="footerTabSelected('search')"
+        :class="{
+          'bg-surface-hover text-primary': activeFooterTab === 'search',
+          'text-white': !activeFooterTab === 'search',
+        }"
+        ><Search
+      /></BaseButton>
     </NuxtLink>
-    <NuxtLink to="wishList">
-      <BaseButton><Heart /></BaseButton>
+    <NuxtLink to="/wishList">
+      <BaseButton
+        @click="footerTabSelected('wishList')"
+        :class="{
+          'bg-surface-hover text-primary': activeFooterTab === 'wishList',
+          'text-white': !activeFooterTab === 'wishList',
+        }"
+        ><Heart
+      /></BaseButton>
     </NuxtLink>
-    <NuxtLink to="user">
-      <BaseButton><User /></BaseButton>
+    <NuxtLink to="/user">
+      <BaseButton
+        @click="footerTabSelected('user')"
+        :class="{
+          'bg-surface-hover text-primary': activeFooterTab === 'user',
+          'text-white': !activeFooterTab === 'user',
+        }"
+        ><User
+      /></BaseButton>
     </NuxtLink>
   </div>
 </template>
+
 <script setup>
 import { LayoutGrid, Search, Heart, User } from "lucide-vue-next";
+const activeFooterTab = ref("");
+
+function footerTabSelected(tab) {
+  console.log("Tab Selected:", tab); // Debugging the function call
+  activeFooterTab.value = tab;
+}
 </script>

877a151cfb0738730bee98743e67c0f8171060f0
AliArefnia <maarefnia1377@gmail.com>
2025-03-29 02:05:25 +0330

    Update signUp.vue

--- pages/signUp.vue
+++ pages/signUp.vue
@@ -1,39 +1,39 @@
 <template>
-  <div
-    class="min-h-screen flex items-center justify-center bg-surface-dark text-white"
-  >
-    <div class="w-full max-w-md bg-surface-card p-6 rounded-lg">
-      <h2 class="text-2xl font-bold mb-6 text-center">Sign Up</h2>
-      <form @submit.prevent="signUp" class="space-y-4">
-        <input
-          v-model="email"
-          type="email"
-          placeholder="Email"
-          required
-          class="w-full px-4 py-2 bg-surface-hover text-white rounded-lg focus:outline-none focus:ring-2 focus:ring-blue-500"
-        />
-        <input
-          v-model="password"
-          type="password"
-          placeholder="Password"
-          required
-          class="w-full px-4 py-2 bg-surface-hover text-white rounded-lg focus:outline-none focus:ring-2 focus:ring-blue-500"
-        />
+  <div class="flex justify-center min-h-[500px]">
+    <div class="flex items-center justify-center bg-surface-dark text-white">
+      <div class="w-11/12 sm:w-full max-w-md bg-surface-card p-6 rounded-lg">
+        <h2 class="text-2xl font-bold mb-6 text-center">Sign Up</h2>
+        <form @submit.prevent="signUp" class="space-y-4">
+          <input
+            v-model="email"
+            type="email"
+            placeholder="Email"
+            required
+            class="w-full px-4 py-2 bg-surface-hover text-white rounded-lg focus:outline-none focus:ring-2 focus:ring-blue-500"
+          />
+          <input
+            v-model="password"
+            type="password"
+            placeholder="Password"
+            required
+            class="w-full px-4 py-2 bg-surface-hover text-white rounded-lg focus:outline-none focus:ring-2 focus:ring-blue-500"
+          />
 
-        <button
-          type="submit"
-          class="w-full bg-blue-600 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded-lg transition duration-300"
-        >
-          Sign Up
-        </button>
-      </form>
-      <p v-if="message" class="mt-4 text-green-400">{{ message }}</p>
-      <p v-if="error" class="mt-4 text-red-400">{{ error }}</p>
+          <button
+            type="submit"
+            class="w-full bg-blue-600 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded-lg transition duration-300"
+          >
+            Sign Up
+          </button>
+        </form>
+        <p v-if="message" class="mt-4 text-green-400">{{ message }}</p>
+        <p v-if="error" class="mt-4 text-red-400">{{ error }}</p>
+      </div>
     </div>
   </div>
 </template>
 
-<script setup>
+<script setup lang="ts">
 definePageMeta({
   layout: "default",
 });
@@ -41,7 +41,6 @@ definePageMeta({
 import { ref } from "vue";
 import { useRouter } from "vue-router";
 import { useUserData } from "~/store/user";
-import { createClient } from "@supabase/supabase-js";
 
 const userData = useUserData();
 const router = useRouter();
@@ -52,12 +51,7 @@ const message = ref("");
 const error = ref("");
 
 async function signUp() {
-  const config = useRuntimeConfig();
-
-  const supabase = createClient(
-    `${config.public.SUPABASE_URL}`,
-    `${config.public.SUPABASE_KEY}`
-  );
+  const supabase: any = useNuxtApp().$supabase;
   try {
     const { data, error } = await supabase.auth.signUp({
       email: email.value,
@@ -67,6 +61,7 @@ async function signUp() {
     console.log(data, error);
 
     if (error) {
+      error.value = error.message;
       return { error: error.message };
     }
 

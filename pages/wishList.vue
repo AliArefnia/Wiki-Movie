969de2a53a25e3cf33e04b20b0f142d9c3a3ecb9
AliArefnia <maarefnia1377@gmail.com>
2025-03-29 02:07:40 +0330

    Update wishList.vue
    1-improve it's UI
    2- fix blink text issue
    3-add redirect from

--- pages/wishList.vue
+++ pages/wishList.vue
@@ -1,14 +1,28 @@
 <template>
   <div class="bg-surface-dark pt-2">
-    <div
-      v-if="wishListMovies?.length === 0 && !isLoading"
-      class="text-gray-400 text-center font-display mt-6"
-    >
-      You can add movies to your wishlist by clicking the heart icon on the
-      movie card.
+    <div v-if="isInitialLoadDone">
+      <div
+        v-if="
+          wishListMovies?.length === 0 && !isLoading && !error && isUserLoggedIn
+        "
+        class="text-gray-400 text-center font-display mt-6"
+      >
+        You can add movies to your wishlist by clicking the heart icon on the
+        movie card.
+      </div>
     </div>
     <div v-if="error" class="text-gray-400 text-center font-display mt-6">
       {{ error }}
+    </div>
+    <div v-if="!isUserLoggedIn" class="flex justify-center">
+      <button
+        @click="
+          () => router.push(`/login?from=${router.currentRoute.value.path}`)
+        "
+        class="mt-6 px-4 py-2 bg-blue-500 text-white rounded hover:bg-blue-600 cursor-pointer"
+      >
+        Log In
+      </button>
     </div>
     <div v-if="isLoading" class="text-gray-400 text-center font-display mt-6">
       Loading...
@@ -36,34 +50,48 @@
 </template>
 
 <script setup lang="ts">
+// import { useInfiniteScroll, useDebounceFn } from "@vueuse/core";
 import type { Movie } from "~/types/types";
 import { useUserData } from "~/store/user";
-
+import { useRouter } from "vue-router";
+const router = useRouter();
 const userData = useUserData();
+// const page = ref(1);
 const isLoading = ref(false);
 const wishListMovies = ref<Movie[] | null>([]);
-const error = ref<string | null>(null);
+const error = ref<string | null>("");
+const isUserLoggedIn = computed(() => userData.isLoggedIn);
+const isInitialLoadDone = ref(false);
+const wishList = ref();
 
 async function getWishListMovies(movieId: Number) {
   try {
-    isLoading.value = true;
     const data = await $fetch<Movie>(`/api/MovieById?movieId=${movieId}`);
     wishListMovies.value?.push(data);
   } catch (err) {
     error.value = "Couldn't fetch wish list movies";
     console.log("error fetching searched movie");
   } finally {
-    isLoading.value = false;
+    isInitialLoadDone.value = true;
   }
 }
 
 onMounted(async () => {
   isLoading.value = true;
-  const wishList = userData.userWishList;
+  try {
+    if (!isUserLoggedIn.value)
+      throw new Error("Please log in to view your wish list.");
 
-  wishList?.map((movieId) => {
-    getWishListMovies(movieId);
-  });
+    wishList.value = userData.userWishList;
+
+    wishList.value?.map((movieId: number) => {
+      getWishListMovies(movieId);
+    });
+  } catch (err) {
+    error.value = (err as Error).message || "Unknown error occurred";
+  } finally {
+    isLoading.value = false;
+  }
 });
 </script>
 

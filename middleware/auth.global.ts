bad02f83fe7d9be914c7276d8b4f9d2682f4c9d0
AliArefnia <maarefnia1377@gmail.com>
2025-03-28 22:01:44 +0330

    add global middleware to load user if user reload the page

---
+++ middleware/auth.global.ts
@@ -0,0 +1,16 @@
+import { useUserData } from "~/store/user";
+
+export default defineNuxtRouteMiddleware(async () => {
+  // skip middleware on server
+  if (process.server) return;
+
+  const userStore = useUserData();
+
+  if (!userStore.isLoggedIn && !userStore.isUserLoaded) {
+    try {
+      await userStore.fetchUser();
+    } catch (err) {
+      console.warn("User not authenticated.");
+    }
+  }
+});

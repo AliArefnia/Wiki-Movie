4df67f3aa8bce5f4f5752a79264751a53653114c
AliArefnia <maarefnia1377@gmail.com>
2025-03-31 23:32:25 +0330

    Fix Footer
    Fix footer to change and keep it's state based on all the related pages

--- components/Footer.vue
+++ components/Footer.vue
@@ -4,52 +4,63 @@
   >
     <NuxtLink to="/"
       ><BaseButton
+        variant="footer"
         @click="footerTabSelected('home')"
-        :class="{
-          'bg-surface-hover text-primary': activeFooterTab === 'home',
-          'text-white': !activeFooterTab === 'home',
-        }"
+        :active="activeFooterTab === 'home'"
         ><LayoutGrid /></BaseButton
     ></NuxtLink>
     <NuxtLink to="/search">
       <BaseButton
+        variant="footer"
         @click="footerTabSelected('search')"
-        :class="{
-          'bg-surface-hover text-primary': activeFooterTab === 'search',
-          'text-white': !activeFooterTab === 'search',
-        }"
+        :active="activeFooterTab === 'search'"
         ><Search
       /></BaseButton>
     </NuxtLink>
     <NuxtLink to="/wishList">
       <BaseButton
+        variant="footer"
         @click="footerTabSelected('wishList')"
-        :class="{
-          'bg-surface-hover text-primary': activeFooterTab === 'wishList',
-          'text-white': !activeFooterTab === 'wishList',
-        }"
+        :active="activeFooterTab === 'wishList'"
         ><Heart
       /></BaseButton>
     </NuxtLink>
     <NuxtLink to="/user">
       <BaseButton
+        variant="footer"
         @click="footerTabSelected('user')"
-        :class="{
-          'bg-surface-hover text-primary': activeFooterTab === 'user',
-          'text-white': !activeFooterTab === 'user',
-        }"
+        :active="activeFooterTab === 'user'"
         ><User
       /></BaseButton>
     </NuxtLink>
   </div>
 </template>
 
-<script setup>
+<script setup lang="ts">
 import { LayoutGrid, Search, Heart, User } from "lucide-vue-next";
+import { useRoute } from "vue-router";
+
+const route = useRoute();
+
 const activeFooterTab = ref("");
 
-function footerTabSelected(tab) {
-  console.log("Tab Selected:", tab); // Debugging the function call
+function footerTabSelected(tab: string) {
   activeFooterTab.value = tab;
 }
+
+watch(
+  () => route.path,
+  (newPath) => {
+    if (newPath.includes("/search") || route.query.from === "search")
+      activeFooterTab.value = "search";
+    else if (newPath.includes("/wishList") || route.query.from === "wishList")
+      activeFooterTab.value = "wishList";
+    else if (newPath.includes("/user") || route.query.from === "user")
+      activeFooterTab.value = "user";
+    else if (newPath.includes("/login") || newPath.includes("/signup"))
+      activeFooterTab.value = "";
+    else activeFooterTab.value = "home";
+  },
+  { immediate: true }
+);
 </script>

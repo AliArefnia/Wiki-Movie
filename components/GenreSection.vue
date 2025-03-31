754ab4429a12aa26b0002d671a58f62bc1d8dfdc
AliArefnia <maarefnia1377@gmail.com>
2025-03-31 23:34:29 +0330

    add genre section and base genre card to have all genres
    currently it's enough to define the necessary genres in the store

---
+++ components/GenreSection.vue
@@ -0,0 +1,17 @@
+<template>
+  <div class="flex overflow-auto py-4 mt-2">
+    <NuxtLink
+      v-for="genre in movieGenres"
+      :key="genre.id"
+      :to="`/genre/${genre.name}`"
+    >
+      <BaseGenreCard class="shrink-0 mx-2" :genreName="genre.name" />
+    </NuxtLink>
+  </div>
+</template>
+<script setup lang="ts">
+import { useMovieStore } from "~/store/store";
+
+const movieStore = useMovieStore();
+const movieGenres = computed(() => movieStore.getMovieGenres);
+</script>
---
+++ components/base/BaseGenreCard.vue
@@ -0,0 +1,15 @@
+<template>
+  <div
+    class="bg-surface-hover text-white flex items-center justify-center rounded-sm overflow-hidden w-[110px] h-[50px] hover:scale-110 transition-transform text-center p-1"
+  >
+    <p class="">{{ genreName }}</p>
+  </div>
+</template>
+
+<script setup lang="ts">
+const props = defineProps<{
+  genreName: string;
+}>();
+</script>
+
+<style scoped></style>

56d3a860222202d311198ec3b9a31f50a5a068a7
AliArefnia <maarefnia1377@gmail.com>
2025-04-08 22:14:37 +0330

    add a dive so can use class on the trailer card

--- components/base/BaseTrailerCard.vue
+++ components/base/BaseTrailerCard.vue
@@ -1,33 +1,37 @@
 <template>
-  <p class="bg-black/50 text-white px-2 mx-4 py-1 rounded text-xs inline-block">
-    {{ props.trailerName }}
-  </p>
+  <div>
+    <p
+      class="bg-black/50 text-white px-2 mx-4 py-1 rounded text-xs inline-block"
+    >
+      {{ props.trailerName }}
+    </p>
 
-  <div
-    class="relative rounded-2xl overflow-hidden font-sans cursor-pointer flex justify-center"
-    :class="{ 'w-full': props.soloMovie, 'w-fit': !props.soloMovie }"
-    @click="showIframe = true"
-  >
-    <transition name="fade" mode="out-in">
-      <img
-        v-if="!showIframe"
-        :src="thumbnailUrl"
-        :width="trailerWidth"
-        :height="trailerHeight"
-        class="object-cover transition-opacity duration-500 rounded-2xl"
-        alt="Trailer Thumbnail"
-      />
-      <iframe
-        v-else
-        :width="trailerWidth"
-        :height="trailerHeight"
-        :src="`https://www.youtube-nocookie.com/embed/${videoId}?autoplay=1&rel=0`"
-        frameborder="0"
-        allow="accelerometer; autoplay; encrypted-media; gyroscope; picture-in-picture; web-share"
-        allowfullscreen
-        class="transition-opacity duration-500 rounded-2xl"
-      ></iframe>
-    </transition>
+    <div
+      class="relative rounded-2xl overflow-hidden font-sans cursor-pointer flex justify-center"
+      :class="{ 'w-full': props.soloMovie, 'w-fit': !props.soloMovie }"
+      @click="showIframe = true"
+    >
+      <transition name="fade" mode="out-in">
+        <img
+          v-if="!showIframe"
+          :src="thumbnailUrl"
+          :width="trailerWidth"
+          :height="trailerHeight"
+          class="object-cover transition-opacity duration-500 rounded-2xl"
+          alt="Trailer Thumbnail"
+        />
+        <iframe
+          v-else
+          :width="trailerWidth"
+          :height="trailerHeight"
+          :src="`https://www.youtube-nocookie.com/embed/${videoId}?autoplay=1&rel=0`"
+          frameborder="0"
+          allow="accelerometer; autoplay; encrypted-media; gyroscope; picture-in-picture; web-share"
+          allowfullscreen
+          class="transition-opacity duration-500 rounded-2xl"
+        ></iframe>
+      </transition>
+    </div>
   </div>
 </template>
 

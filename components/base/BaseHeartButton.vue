51e119d189562d2c23c837b5ef9e05a284836969
AliArefnia <maarefnia1377@gmail.com>
2025-04-08 22:11:57 +0330

    Create BaseHeartButton.vue
    to separate the heart button and it's funtionality

---
+++ components/base/BaseHeartButton.vue
@@ -0,0 +1,31 @@
+<template>
+  <button @click="toggleMovieWishList()">
+    <Heart
+      :size="50"
+      :class="[
+        isInWishList ? 'fill-red-500' : 'fill-none stroke-gray-600',
+        'transition-transform duration-300 ease-in-out',
+        isInWishList ? 'scale-110 brightness-125' : 'scale-100 brightness-100',
+        isInWishList ? 'animate-pulse' : '',
+      ]"
+      class=""
+      color="red"
+    />
+  </button>
+</template>
+
+<script setup lang="ts">
+import { Heart } from "lucide-vue-next";
+import BaseAlertModule from "./BaseAlertModule.vue";
+const props = defineProps<{
+  isInWishList: boolean;
+}>();
+
+const emit = defineEmits<{
+  (e: "toggleMovieWishList"): void;
+}>();
+
+function toggleMovieWishList() {
+  emit("toggleMovieWishList");
+}
+</script>

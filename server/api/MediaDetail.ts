0b81ff6ce6ee045d328cdd8b117e1507ce2bc7a7
AliArefnia <maarefnia1377@gmail.com>
2025-04-17 23:52:20 +0330

    get multi media type detail instead of only getting movie detail

--- server/api/MediaDetail.ts
+++ server/api/MediaDetail.ts
@@ -1,12 +1,16 @@
 import type { MovieDetail } from "~/types/types";
 
 export default defineEventHandler(async (event) => {
-  const { movieId } = getQuery(event);
+  const { mediaType, id } = getQuery(event);
   const config = useRuntimeConfig();
+
+  if (!mediaType || !id) {
+    return { error: "Missing mediaType or id" };
+  }
 
   try {
     const response = await $fetch<MovieDetail>(
-      `https://api.themoviedb.org/3/movie/${movieId}?language=en-US`,
+      `https://api.themoviedb.org/3/${mediaType}/${id}?language=en-US`,
       {
         method: "GET",
         headers: {
@@ -16,12 +20,13 @@ export default defineEventHandler(async (event) => {
       }
     );
 
-    return {
-      ...response,
-      vote_average: Number(response.vote_average.toFixed(1) || 0),
-      popularity: Number(response.popularity.toFixed(2) || 0),
-      release_date: response.release_date.slice(0, 4) || "N/A",
-    };
+    if (mediaType === "movie" || mediaType === "tv") {
+      response.vote_average = Number(response.vote_average.toFixed(1) || 0);
+      response.popularity = Number(response.popularity.toFixed(2) || 0);
+      response.release_date = response.release_date?.slice(0, 4) || "N/A";
+    }
+
+    return response;
   } catch (error) {
     console.error("Error fetching movie Detail:", error);
     return { error: "Failed to fetch movie Detail" };

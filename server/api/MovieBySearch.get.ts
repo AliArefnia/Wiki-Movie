585e61484dcc28d5adf87cdfdf2d89c0aa35bf56
AliArefnia <maarefnia1377@gmail.com>
2025-04-19 23:56:30 +0330

    get search results using multi api to get all type of data

--- server/api/MovieBySearch.get.ts
+++ server/api/MovieBySearch.get.ts
@@ -1,4 +1,4 @@
-import type { MovieGet } from "~/types/types";
+import type { MovieGet, SearchResult } from "~/types/types";
 
 export default defineEventHandler(async (event) => {
   try {
@@ -7,8 +7,8 @@ export default defineEventHandler(async (event) => {
 
     const IMAGE_URL = `https://image.tmdb.org/t/p/w${width}`;
 
-    const response = await $fetch<MovieGet>(
-      `https://api.themoviedb.org/3/search/movie?query=${searchTerm}&include_adult=false&language=en-US&page=${page}`,
+    const response = await $fetch<{ results: SearchResult[] }>(
+      `https://api.themoviedb.org/3/search/multi?query=${searchTerm}&include_adult=false&language=en-US&page=${page}`,
       {
         method: "GET",
         headers: {
@@ -17,16 +17,57 @@ export default defineEventHandler(async (event) => {
         },
       }
     );
-    return response.results.map(
-      ({ id, title, vote_average, poster_path, release_date, genre_ids }) => ({
-        id,
-        title: title ?? "Untitled",
-        vote_average: Number(vote_average.toFixed(1) || 0),
-        poster_path: `${IMAGE_URL}${poster_path}`,
-        release_date: release_date.slice(0, 4) || "N/A",
-        genre_ids: genre_ids || [],
+
+    return response.results
+      .map((item: any): SearchResult | null => {
+        if (item.media_type === "movie") {
+          return {
+            id: item.id,
+            media_type: "movie",
+            title: item.title || "Untitled",
+            vote_average: Number((item.vote_average ?? 0).toFixed(1)),
+            poster_path: item.poster_path
+              ? `${IMAGE_URL}${item.poster_path}`
+              : null,
+            release_date: (item.release_date || "N/A").slice(0, 4),
+            genre_ids: item.genre_ids || [],
+          };
+        } else if (item.media_type === "tv") {
+          return {
+            id: item.id,
+            media_type: "tv",
+            name: item.name || "Untitled",
+            vote_average: Number((item.vote_average ?? 0).toFixed(1)),
+            poster_path: item.poster_path
+              ? `${IMAGE_URL}${item.poster_path}`
+              : null,
+            release_date: (item.first_air_date || "N/A").slice(0, 4),
+            genre_ids: item.genre_ids || [],
+          };
+        } else if (item.media_type === "person") {
+          return {
+            id: item.id,
+            media_type: "person",
+            name: item.name,
+            profile_path: item.profile_path
+              ? `${IMAGE_URL}${item.profile_path}`
+              : null,
+            known_for: item.known_for.map((kf: any) => ({
+              id: kf.id,
+              title: kf.title,
+              name: kf.name,
+              media_type: kf.media_type,
+              poster_path: kf.poster_path
+                ? `${IMAGE_URL}${kf.poster_path}`
+                : null,
+            })),
+            known_for_department: item.known_for_department,
+          };
+        }
+
+        return null;
       })
-    );
+      .filter((item): item is SearchResult => item !== null);
   } catch (error) {
     console.log(error);
     console.error("Error fetching movies by search:", error);

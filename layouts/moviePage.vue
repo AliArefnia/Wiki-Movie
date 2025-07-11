1537acce524f6a50b0a27b3dba4a3a2bd1a38348
AliArefnia <maarefnia1377@gmail.com>
2025-07-11 17:21:52 +0330

    add movie page layout

---
+++ layouts/moviePage.vue
@@ -0,0 +1,14 @@
+<template>
+  <div class="min-h-screen flex flex-col">
+    <HeaderSectionGenrePage />
+    <main class="bg-surface-dark flex-grow relative">
+      <slot></slot>
+    </main>
+    <Footer />
+  </div>
+</template>
+
+<script setup>
+import HeaderSectionGenrePage from "~/components/HeaderSectionGenrePage.vue";
+import Footer from "~/components/Footer.vue";
+</script>

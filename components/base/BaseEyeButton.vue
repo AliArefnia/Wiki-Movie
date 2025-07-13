06461fa4cc50f11436157462cd1d7f0de9210fdd
AliArefnia <maarefnia1377@gmail.com>
2025-07-13 16:17:12 +0330

    Create watch button to add a movie to watch list

---
+++ components/base/BaseEyeButton.vue
@@ -0,0 +1,30 @@
+<template>
+  <button @click="toggleMovieWatched()">
+    <Eye
+      v-if="isWatched"
+      class="hover:cursor-pointer hover:scale-105 hover:stroke-red-500"
+      color="red"
+    ></Eye>
+    <EyeClosed
+      v-else
+      class="hover:cursor-pointer hover:scale-105 hover:stroke-red-500"
+    ></EyeClosed>
+  </button>
+</template>
+
+<script setup lang="ts">
+import { Eye } from "lucide-vue-next";
+import { EyeClosed } from "lucide-vue-next";
+
+const props = defineProps<{
+  isWatched: boolean;
+}>();
+
+const emit = defineEmits<{
+  (e: "toggleMovieWatched"): void;
+}>();
+
+function toggleMovieWatched() {
+  emit("toggleMovieWatched");
+}
+</script>

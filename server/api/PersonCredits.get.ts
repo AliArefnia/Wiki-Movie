1109b21f5b597a1c50d1db527e89afb7a0a40dbd
AliArefnia <maarefnia1377@gmail.com>
2025-07-14 16:07:57 +0330

    add getting person credits API
     (only as a cast now)

---
+++ server/api/PersonCredits.get.ts
@@ -0,0 +1,52 @@
+import { CombinedCreditsResponse, Movie, TvShow } from "~/types/types";
+
+export default defineEventHandler(async (event) => {
+  const { id } = getQuery(event);
+  const config = useRuntimeConfig();
+
+  if (!id) {
+    return { error: "Missing person ID" };
+  }
+
+  const headers = {
+    accept: "application/json",
+    Authorization: `Bearer ${config.public.TMDB_API_KEY}`,
+  };
+
+  try {
+    const url = `https://api.themoviedb.org/3/person/${id}/combined_credits?language=en-US`;
+    const data = await $fetch<CombinedCreditsResponse>(url, { headers });
+
+    const personCredits = data.cast.map((item) => {
+      if (item.media_type === "movie") {
+        const movie: Movie = {
+          id: item.id,
+          media_type: "movie",
+          title: item.title ?? "Untitled",
+          vote_average: Number(item.vote_average?.toFixed(1) || 0),
+          poster_path: item.poster_path ?? null,
+          release_date: item.release_date?.slice(0, 4) ?? "N/A",
+          genre_ids: item.genre_ids ?? [],
+        };
+        return movie;
+      } else {
+        const tvShow: TvShow = {
+          id: item.id,
+          media_type: "tv",
+          name: item.name ?? "Untitled",
+          vote_average: Number(item.vote_average?.toFixed(1) || 0),
+          poster_path: item.poster_path ?? null,
+          first_air_date: item.first_air_date?.slice(0, 4) ?? "N/A",
+          genre_ids: item.genre_ids ?? [],
+        };
+        return tvShow;
+      }
+    });
+    return { success: true, data: personCredits };
+  } catch (error) {
+    return {
+      success: false,
+      error: "Failed to fetch credits for person",
+    } as any;
+  }
+});

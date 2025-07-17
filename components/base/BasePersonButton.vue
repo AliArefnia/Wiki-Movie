d8bbede0176178689bb4629dc2f564c57c521bc9
AliArefnia <maarefnia1377@gmail.com>
2025-07-17 23:35:20 +0330

    Create BasePersonButton.vue
    using this button user can add a person to it's favourite list

---
+++ components/base/BasePersonButton.vue
@@ -0,0 +1,29 @@
+<template>
+  <button @click="toggleFavouritePerson()">
+    <UserMinus
+      v-if="isFavouritePerson"
+      class="hover:cursor-pointer hover:scale-105 hover:stroke-red-500 transition-all duration-300"
+      color="red"
+    ></UserMinus>
+    <UserPlus
+      v-else
+      class="hover:cursor-pointer hover:scale-105 hover:stroke-red-500 transition-all duration-300"
+    ></UserPlus>
+  </button>
+</template>
+
+<script setup lang="ts">
+import { UserMinus, UserPlus } from "lucide-vue-next";
+
+const props = defineProps<{
+  isFavouritePerson: boolean;
+}>();
+
+const emit = defineEmits<{
+  (e: "toggleFavouritePerson"): void;
+}>();
+
+function toggleFavouritePerson() {
+  emit("toggleFavouritePerson");
+}
+</script>

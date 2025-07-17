20944d23e60c9ba29bb6f90a4ca0c421fcc9ad5b
AliArefnia <maarefnia1377@gmail.com>
2025-07-18 00:25:17 +0330

    add favorite person section

--- pages/User/lists.vue
+++ pages/User/lists.vue
@@ -15,7 +15,20 @@
     </div>
 
     <UserListPage
-      :mediaList="activeTab === 'wishlist' ? wishListMedia : watchListMedia"
+      :mediaList="
+        activeTab === 'wishlist'
+          ? wishListMedia
+          : activeTab === 'watchlist'
+          ? watchListMedia
+          : favouritePersonListMedia
+      "
+      :type="
+        activeTab === 'wishlist'
+          ? 'movie'
+          : activeTab === 'watchlist'
+          ? 'movie'
+          : 'person'
+      "
       :loading="isLoading"
       :error="error"
       :isUserLoggedIn="isUserLoggedIn"
@@ -24,7 +37,9 @@
       :emptyText="
         activeTab === 'wishlist'
           ? 'You can add movies to your wishlist by clicking the heart icon in the movie page.'
-          : 'You can add movies to your Watch List by clicking the Eye icon in the movie page.'
+          : activeTab === 'watchlist'
+          ? 'You can add movies to your Watch List by clicking the Eye icon in the movie page.'
+          : 'You can add your favorite person to your favourite person List by clicking the person icon in the person page.'
       "
     />
   </div>
@@ -40,7 +55,13 @@ useHead({ title: "My Lists" });
 import { useUserData } from "~/store/user";
 import { useRouter, useRoute } from "vue-router";
 import UserListPage from "~/components/User/UserListPage.vue";
-import type { MediaItem, WishListItem, WatchListItem } from "~/types/types";
+import type {
+  MediaItem,
+  WishListItem,
+  WatchListItem,
+  Person,
+  FavouriePersonteItem,
+} from "~/types/types";
 
 const router = useRouter();
 const route = useRoute();
@@ -49,24 +70,26 @@ const userData = useUserData();
 const tabs = [
   { label: "Wishlist", key: "wishlist" },
   { label: "Watchlist", key: "watchlist" },
+  { label: "Fav Person", key: "favouritePerson" },
 ];
 
-const activeTab = ref<"wishlist" | "watchlist">("wishlist");
+const activeTab = ref<"wishlist" | "watchlist" | "favouritePerson">("wishlist");
 
 const wishListMedia = ref<MediaItem[]>([]);
 const watchListMedia = ref<MediaItem[]>([]);
+const favouritePersonListMedia = ref<Person[]>([]);
 const error = ref<string | null>(null);
 const isLoading = ref(false);
 const isInitialLoadDone = ref(false);
 const isUserLoggedIn = computed(() => userData.isLoggedIn);
 
-async function getMedia(mediaId: number, mediaType: "tv" | "movie") {
-  return await $fetch<MediaItem>(
+async function getMedia(mediaId: number, mediaType: "tv" | "movie" | "person") {
+  return await $fetch<MediaItem | Person>(
     `/api/MediaById?mediaId=${mediaId}&mediaType=${mediaType}`
   );
 }
 
-function selectTab(tabKey: "wishlist" | "watchlist") {
+function selectTab(tabKey: "wishlist" | "watchlist" | "favouritePerson") {
   activeTab.value = tabKey;
   router.push({ query: { ...route.query, tab: tabKey } });
 }
@@ -76,7 +99,7 @@ onMounted(async () => {
   try {
     if (!isUserLoggedIn.value) throw new Error("Please sign in first.");
 
-    const [wish, watch] = await Promise.all([
+    const [wish, watch, faVourite] = await Promise.all([
       Promise.all(
         (userData.GetUserWishList ?? []).map((item: WishListItem) =>
           getMedia(item.id, item.mediaType).catch(() => null)
@@ -87,10 +110,17 @@ onMounted(async () => {
           getMedia(item.id, item.mediaType).catch(() => null)
         )
       ),
+      Promise.all(
+        (userData.GetUserFavouritePersonList ?? []).map(
+          (item: FavouriePersonteItem) =>
+            getMedia(item.id, item.mediaType).catch(() => null)
+        )
+      ),
     ]);
 
     wishListMedia.value = wish.filter(Boolean) as MediaItem[];
     watchListMedia.value = watch.filter(Boolean) as MediaItem[];
+    favouritePersonListMedia.value = faVourite.filter(Boolean) as Person[];
   } catch (err) {
     error.value = (err as Error).message;
   } finally {
@@ -99,7 +129,7 @@ onMounted(async () => {
   }
 
   const tab = route.query.tab;
-  if (tab === "wishlist" || tab === "watchlist") {
+  if (tab === "wishlist" || tab === "watchlist" || tab === "favouritePerson") {
     activeTab.value = tab;
   } else {
     router.replace({ query: { ...route.query, tab: "wishlist" } });

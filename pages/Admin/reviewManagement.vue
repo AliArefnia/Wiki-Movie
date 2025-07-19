ac83cd27ec70b8c2d0a6d3e85b0cba3802bed79b
AliArefnia <maarefnia1377@gmail.com>
2025-07-19 17:12:24 +0330

    delete reviews page and add review management which use admin dynamic table

---
+++ pages/Admin/reviewManagement.vue
@@ -0,0 +1,31 @@
+<template>
+  <div class="px-4">
+    <h1 class="text-white text-2xl font-bold ml-5 mb-4 pt-5 font-display">
+      Review Moderation
+    </h1>
+    <AdminTable
+      title="Moderate Reviews"
+      :headers="headers"
+      endpoint="/api/admin/getReviews"
+      update-endpoint="/api/admin/updateReviewsStatus"
+      status-filter
+      moderation-mode
+      has-status
+      has-date
+      expand-field="comment"
+      expand-label="Comment"
+    />
+  </div>
+</template>
+
+<script setup lang="ts">
+import AdminTable from "~/components/Admin/AdminTable.vue";
+
+const headers = [
+  { text: "User Name/ID", value: "user.name" },
+  { text: "Comment", value: "comment" },
+  { text: "Date", value: "created_at" },
+  { text: "Status", value: "status" },
+  { text: "Operations", value: "operation", fixed: true },
+];
+</script>

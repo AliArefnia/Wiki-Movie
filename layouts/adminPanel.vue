2aa9d24d3d4a6d998828418d1f1cc5868cedc339
AliArefnia <maarefnia1377@gmail.com>
2025-07-20 23:03:00 +0330

    create admin panel layout

---
+++ layouts/adminPanel.vue
@@ -0,0 +1,21 @@
+<template>
+  <div class="min-h-screen bg-slate-900 text-white flex flex-col">
+    <!-- Sidebar -->
+    <AdminDashboardHeader></AdminDashboardHeader>
+    <main class="bg-surface-dark flex-grow">
+      <slot></slot>
+    </main>
+    <Footer role="admin" />
+  </div>
+</template>
+
+<script setup lang="ts">
+import AdminDashboardHeader from "~/components/AdminDashboardHeader.vue";
+import Footer from "~/components/Footer.vue";
+import { useUserData } from "~/store/user";
+
+const userStore = useUserData();
+console.log(userStore);
+const role = computed(() => userStore.GetUserRole);
+console.log(role.value);
+</script>

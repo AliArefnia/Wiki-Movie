58e7de94ba77a008472dbf661177e4534e58967a
AliArefnia <maarefnia1377@gmail.com>
2025-07-21 17:11:02 +0330

    remove role from the layouts and add it to the footer itself

--- layouts/adminPanel.vue
+++ layouts/adminPanel.vue
@@ -5,17 +5,11 @@
     <main class="bg-surface-dark flex-grow">
       <slot></slot>
     </main>
-    <Footer role="admin" />
+    <Footer />
   </div>
 </template>
 
 <script setup lang="ts">
 import AdminDashboardHeader from "~/components/AdminDashboardHeader.vue";
 import Footer from "~/components/Footer.vue";
-import { useUserData } from "~/store/user";
-
-const userStore = useUserData();
-console.log(userStore);
-const role = computed(() => userStore.GetUserRole);
-console.log(role.value);
 </script>

e12a92ffaec38893926f069d60cab24c014620a6
AliArefnia <maarefnia1377@gmail.com>
2025-07-22 23:29:09 +0330

    Update index.vue
    update UI

--- pages/Admin/index.vue
+++ pages/Admin/index.vue
@@ -9,25 +9,28 @@
 
       <!-- Optional: Overview section -->
       <div class="grid grid-cols-1 md:grid-cols-2 gap-6">
-        <div class="bg-slate-800 p-4 rounded shadow group">
+        <div class="bg-surface-card p-4 rounded shadow group">
           <h2 class="text-lg font-semibold mb-2">👥 Users</h2>
           <p class="text-sm text-gray-400">
             Manage registered users, delete accounts, and more.
           </p>
           <NuxtLink
             to="/admin/users"
-            class="inline-block mt-3 text-blue-400 hover:underline"
+            class="inline-block mt-3 text-blue-400 hover:scale-105 transition-transform"
           >
             Go to User Management
             <span class="group-hover:ml-2 transition-all duration-300">→</span>
           </NuxtLink>
         </div>
-        <div class="bg-slate-800 p-4 rounded shadow group">
+        <div class="bg-surface-card p-4 rounded shadow group">
           <h2 class="text-lg font-semibold mb-2">📝 Reviews</h2>
           <p class="text-sm text-gray-400">
             Moderate user comments and change review statuses.
           </p>
-          <NuxtLink to="/admin/reviews" class="inline-block mt-3 text-blue-400">
+          <NuxtLink
+            to="/admin/reviews"
+            class="inline-block mt-3 text-blue-400 hover:scale-105 transition-transform"
+          >
             Go to Review Moderation
             <span class="group-hover:ml-2 transition-all duration-300">→</span>
           </NuxtLink>

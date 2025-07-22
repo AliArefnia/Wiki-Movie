fafa603c77080319b5fca7fc86d834031639f323
AliArefnia <maarefnia1377@gmail.com>
2025-07-23 00:14:55 +0330

    fetch movie and tv genres by app mount

--- app.vue
+++ app.vue
@@ -4,7 +4,18 @@
   </NuxtLayout>
 </template>
 
-<script setup>
+<script setup lang="ts">
+import { useMovieStore } from "~/store/store";
+import type { Genre } from "~/types/types";
+const movieStore = useMovieStore();
+
+onBeforeMount(async () => {
+  const movieGenres = await $fetch<Genre[]>("/api/GetGenres?type=movie");
+  movieStore.setMovieGenres(movieGenres);
+  const tvGenres = await $fetch<Genre[]>("/api/GetGenres?type=tv");
+  movieStore.setTvGenres(tvGenres);
+});
+
 useHead({
   titleTemplate: (titleChunk) => {
     return titleChunk ? `Wiki Movie | ${titleChunk} ` : "Wiki Movie";

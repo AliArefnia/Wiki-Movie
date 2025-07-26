29d9c789543da7e34067f69b4cba27a19b275855
AliArefnia <maarefnia1377@gmail.com>
2025-07-26 17:54:41 +0330

    preload inter font

--- layouts/default.vue
+++ layouts/default.vue
@@ -11,4 +11,16 @@
 <script setup>
 import HeaderSection from "~/components/HeaderSection.vue";
 import Footer from "~/components/Footer.vue";
+
+useHead({
+  link: [
+    {
+      rel: "preload",
+      as: "font",
+      href: "/fonts/inter-v19-latin-regular.woff2",
+      type: "font/woff2",
+      crossorigin: "anonymous",
+    },
+  ],
+});
 </script>

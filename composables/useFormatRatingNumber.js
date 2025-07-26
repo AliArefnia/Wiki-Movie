abb44ebb5f938124cb52241bb1f4c1b813f9576b
AliArefnia <maarefnia1377@gmail.com>
2025-07-26 17:51:17 +0330

    Delete useFormatRatingNumber.js

--- composables/useFormatRatingNumber.js
+++
@@ -1,9 +0,0 @@
-export function useFormatNumber() {
-  function formatNumber(num) {
-    return Number.isInteger(num) ? num : parseFloat(num.toFixed(1));
-  }
-
-  return {
-    formatNumber,
-  };
-}

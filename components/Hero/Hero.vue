2c1b8794c22869cb58114c9daf3b10c98fd15273
AliArefnia <maarefnia1377@gmail.com>
2025-07-28 14:26:05 +0330

    use base carousel

--- components/Hero/Hero.vue
+++ components/Hero/Hero.vue
@@ -1,33 +1,8 @@
 <template>
-  <div
-    @mouseenter="stopAutoScroll"
-    @mouseleave="startAutoScroll"
-    @pointerdown="stopAutoScroll"
-    @pointerup="startAutoScroll"
-    class="relative px-0 md:px-8"
-  >
-    <!-- Left Btn -->
-    <button
-      @click="scrollLeft"
-      class="absolute left-0 top-1/2 transform -translate-y-1/2 z-10 bg-gray-900/50 text-white p-2 rounded-full hover:bg-gray-900 transition hidden md:block hover:cursor-pointer"
-    >
-      <CircleChevronLeft :size="CAROUSEL_BUTTON_WIDTH" />
-    </button>
-
-    <!-- Right Btn  -->
-    <button
-      @click="scrollRight"
-      class="absolute right-0 top-1/2 transform -translate-y-1/2 z-10 bg-gray-900/50 text-white p-2 rounded-full hover:bg-gray-900 transition hidden md:block hover:cursor-pointer"
-    >
-      <CircleChevronRight :size="CAROUSEL_BUTTON_WIDTH" />
-    </button>
-
+  <div id="hero" class="relative px-0 md:px-8">
     <!-- Carousel -->
-    <div
-      ref="carousel"
-      class="flex overflow-x-auto py-4 scrollbar-hide scroll-smooth"
-      @scroll="updateScrollState"
-    >
+
+    <BaseCarousel :autoScroll="!!true">
       <NuxtLink
         v-for="media in HotMedia"
         :key="media.id"
@@ -35,6 +10,7 @@
         class="shrink-0 mx-2 group relative overflow-hidden rounded-xl"
       >
         <BaseHeroCard
+          id="herocard"
           :movieTitle="getTitle(media)"
           :rating="media.vote_average"
           :posterUrl="media.poster_path || '/images/placeholder.png'"
@@ -48,22 +24,16 @@
           ⭐ {{ media.vote_average.toFixed(1) }}
         </div>
       </NuxtLink>
-    </div>
+    </BaseCarousel>
   </div>
 </template>
 
 <script setup lang="ts">
 import BaseHeroCard from "./BaseHeroCard.vue";
+import BaseCarousel from "../base/BaseCarousel.vue";
 import type { HotMedia } from "@/types/types";
-import { CircleChevronLeft, CircleChevronRight } from "lucide-vue-next";
 
 const HotMedia = ref<HotMedia[]>([]);
-const carousel = ref<HTMLElement | null>(null);
-const SCROLL_AMOUNT = 300;
-const SCROLL_INTERVAL = 3000;
-const CAROUSEL_BUTTON_WIDTH = 48;
-let autoScrollTimer: NodeJS.Timeout;
-
 const imageWidth = ref(0);
 
 function getCardWidth() {
@@ -83,41 +53,6 @@ onBeforeUnmount(() => {
   window.removeEventListener("resize", updateWidth);
 });
 
-const scrollLeft = () => {
-  if (carousel.value) {
-    carousel.value.scrollBy({ left: -SCROLL_AMOUNT, behavior: "smooth" });
-  }
-};
-
-const scrollRight = () => {
-  if (!carousel.value) return;
-  if (isAtEnd.value) {
-    carousel.value.scrollTo({ left: 0, behavior: "smooth" });
-  } else {
-    carousel.value.scrollBy({ left: SCROLL_AMOUNT, behavior: "smooth" });
-  }
-};
-
-const startAutoScroll = () => {
-  clearInterval(autoScrollTimer);
-  autoScrollTimer = setInterval(() => {
-    scrollRight();
-  }, SCROLL_INTERVAL);
-};
-
-const stopAutoScroll = () => {
-  clearInterval(autoScrollTimer);
-};
-
-// Update scroll position and adjust for rounding issue
-const isAtEnd = ref(false);
-const updateScrollState = () => {
-  if (!carousel.value) return;
-  isAtEnd.value =
-    carousel.value.scrollLeft + carousel.value.clientWidth >=
-    carousel.value.scrollWidth - 10;
-};
-
 const getTitle = (item: HotMedia) =>
   item.media_type === "movie" ? item.title : item.name;
 
@@ -135,19 +70,6 @@ async function fetchAndUpdate() {
 
 onMounted(async () => {
   imageWidth.value = getCardWidth();
-  // add touchEnd and start
-  startAutoScroll();
-  if (carousel.value) {
-    carousel.value.addEventListener("scroll", updateScrollState);
-
-    carousel.value.addEventListener("touchstart", stopAutoScroll, {
-      passive: true,
-    });
-    carousel.value.addEventListener("touchend", startAutoScroll, {
-      passive: true,
-    });
-  }
-
   try {
     const cache = localStorage.getItem("HotMedia");
     if (cache) {
@@ -174,16 +96,8 @@ onMounted(async () => {
     console.error("Error fetching data:", error);
   }
 });
-
-onBeforeUnmount(() => {
-  clearInterval(autoScrollTimer);
-  const el = carousel.value;
-  if (!el) return;
-
-  el.removeEventListener("touchstart", stopAutoScroll);
-  el.removeEventListener("touchend", startAutoScroll);
-});
 </script>
+
 <style scoped>
 .scrollbar-hide::-webkit-scrollbar {
   display: none;

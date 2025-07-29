b1cc386462353317772cc4810acf372207b95a85
AliArefnia <maarefnia1377@gmail.com>
2025-07-29 22:00:08 +0330

    remove preload fonts and use new section names

--- layouts/default.vue
+++ layouts/default.vue
@@ -1,6 +1,6 @@
 <template>
   <div class="min-h-screen flex flex-col">
-    <HeaderSection class="flex" />
+    <Header class="flex" />
     <main class="bg-surface-dark flex-grow">
       <slot></slot>
     </main>
@@ -9,18 +9,6 @@
 </template>
 
 <script setup>
-import HeaderSection from "~/components/HeaderSection.vue";
-import Footer from "~/components/Footer.vue";
-
-useHead({
-  link: [
-    {
-      rel: "preload",
-      as: "font",
-      href: "/fonts/oswald-v56-latin-regular.woff2",
-      type: "font/woff2",
-      crossorigin: "anonymous",
-    },
-  ],
-});
+import Header from "~/components/layoutSections/Header.vue";
+import Footer from "~/components/layoutSections/Footer.vue";
 </script>

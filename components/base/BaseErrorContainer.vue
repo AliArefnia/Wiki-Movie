cc456d043d403580006c83778d435ca3d1d2b0cc
AliArefnia <maarefnia1377@gmail.com>
2025-07-30 19:51:00 +0330

    add error container to get error and refresh function

---
+++ components/base/BaseErrorContainer.vue
@@ -0,0 +1,22 @@
+<template>
+  <div
+    class="mx-auto mt-6 max-w-md rounded-md border border-primary p-4 text-primary shadow-sm bg-surface-card"
+  >
+    <div class="flex items-center justify-between">
+      <p class="font-semibold text-white">Something went wrong</p>
+      <BaseButton @click="refresh" variant="accent" size="sm">
+        Try Again
+      </BaseButton>
+    </div>
+    <p class="mt-2 text-sm italic">
+      {{ error?.message || "Unknown error occurred." }}
+    </p>
+  </div>
+</template>
+
+<script setup lang="ts">
+defineProps<{
+  error: Error | null;
+  refresh: () => void;
+}>();
+</script>

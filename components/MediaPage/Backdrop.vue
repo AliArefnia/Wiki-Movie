798010ea59b9f5cf226f0efa2416ed358d234431
AliArefnia <maarefnia1377@gmail.com>
2025-07-31 16:20:01 +0330

    create backdrop component and reuse it in the page
    also remove extra logic and move it to the component

---
+++ components/MediaPage/Backdrop.vue
@@ -0,0 +1,56 @@
+<template>
+  <div v-if="backdropUrl" class="absolute w-full overflow-hidden">
+    <img
+      :src="backdropUrl"
+      alt="Backdrop"
+      class="w-full h-full sm:h-auto object-cover"
+    />
+    <!-- overlay -->
+    <div
+      class="absolute bottom-0 left-0 w-full h-1/2 pointer-events-none"
+      style="
+        background: linear-gradient(
+          to bottom,
+          rgba(0, 0, 0, 0) 50%,
+          rgba(17, 24, 39, 1) 100%
+        );
+      "
+    ></div>
+  </div>
+</template>
+<script setup lang="ts">
+const props = defineProps<{
+  backdropPath: string;
+}>();
+
+const imageWidth = ref(0);
+
+function getCardWidth() {
+  const vw = window.innerWidth;
+  if (vw >= 1024) return 1280;
+  if (vw >= 780) return 780;
+  return 500;
+}
+
+function updateWidth() {
+  imageWidth.value = getCardWidth();
+}
+
+const debouncedUpdateWidth = useDebounce(updateWidth, 300);
+
+const backdropUrl = computed(() => {
+  if (props.backdropPath && imageWidth.value > 0) {
+    return `https://image.tmdb.org/t/p/w${imageWidth.value}${props.backdropPath}`;
+  }
+  return null;
+});
+
+onMounted(() => {
+  updateWidth();
+  window.addEventListener("resize", debouncedUpdateWidth);
+});
+
+onBeforeUnmount(() => {
+  window.removeEventListener("resize", debouncedUpdateWidth);
+});
+</script>
--- pages/[movieId].vue
+++ pages/[movieId].vue
@@ -1,27 +1,11 @@
 <template>
   <div>
     <div v-if="mediaDetail" class="relative min-h-screen text-white">
-      <div
-        v-if="mediaType === 'movie' || mediaType === 'tv'"
-        class="absolute w-full overflow-hidden"
-      >
-        <img
-          :src="backdropUrl"
-          alt="Backdrop"
-          class="w-full h-full sm:h-auto object-cover"
-        />
-        <!-- Dark fade overlay at bottom 10% -->
-        <div
-          class="absolute bottom-0 left-0 w-full h-1/2 pointer-events-none"
-          style="
-            background: linear-gradient(
-              to bottom,
-              rgba(0, 0, 0, 0) 50%,
-              rgba(17, 24, 39, 1) 100%
-            );
-          "
-        ></div>
-      </div>
+      <!-- Backdrop -->
+      <Backdrop
+        v-if="(mediaType === 'movie' || mediaType === 'tv') && backdropPath"
+        :backdrop-path="backdropPath"
+      ></Backdrop>
 
       <!-- Content Container -->
       <div
@@ -110,6 +94,7 @@ import BaseMediaDetail from "~/components/base/BaseMediaDetail.vue";
 import BaseLoader from "~/components/base/BaseLoader.vue";
 import CommentSection from "~/components/MovieSections/CommentSection.vue";
 import SectionLazy from "~/components/SectionLazy.vue";
+import Backdrop from "~/components/MediaPage/Backdrop.vue";
 
 import type {
   MovieDetail,
@@ -228,23 +213,13 @@ onMounted(async () => {
   }
 });
 
-const backdropUrl = computed(() => {
+const backdropPath = computed(() => {
   if (
     mediaDetail.value &&
     "backdrop_path" in mediaDetail.value &&
     mediaDetail.value.backdrop_path
-  ) {
-    if (window.innerWidth > 1200) {
-      return `https://image.tmdb.org/t/p/w1280${mediaDetail.value.backdrop_path}`;
-    }
-    if (window.innerWidth > 780) {
-      return `https://image.tmdb.org/t/p/w1280${mediaDetail.value.backdrop_path}`;
-    }
-    if (window.innerWidth < 700) {
-      return `https://image.tmdb.org/t/p/w500${mediaDetail.value.backdrop_path}`;
-    }
-  }
-  return "/placeholder.jpg";
+  )
+    return mediaDetail.value.backdrop_path;
 });
 
 definePageMeta({

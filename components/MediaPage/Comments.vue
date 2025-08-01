0fed6bb0ffed5691e6bf82f13d4498c9b4e5c31f
AliArefnia <maarefnia1377@gmail.com>
2025-08-01 14:33:21 +0330

    remake comments
    change folder
    fetch comments on the top level and handle status and error
    also improve UI

--- components/MediaPage/Comments.vue
+++ components/MediaPage/Comments.vue
@@ -17,6 +17,7 @@
         <div class="mt-4 text-right">
           <BaseButton
             variant="accent"
+            size="sm"
             @click="submitComment"
             :disabled="!newComment.trim() || isSubmitting"
           >
@@ -38,50 +39,66 @@
       </template>
     </div>
 
+    <!-- Comments -->
+    <div
+      v-if="status === 'pending'"
+      class="flex justify-center items-center mt-6"
+    >
+      <BaseLoader message="Fetching Comments..." />
+    </div>
+
+    <!-- Error -->
+    <div v-else-if="error" class="flex justify-center items-center">
+      <BaseErrorContainer :error="error" :refresh="refresh" />
+    </div>
+
+    <!-- Success -->
+    <div v-if="visibleComments">
+      <!-- Comments List -->
+      <div class="space-y-4">
+        <BaseCommentCard
+          v-for="comment in visibleComments"
+          :key="comment.id"
+          :comment="comment"
+        />
+      </div>
+
+      <!-- Show More -->
+      <div
+        v-if="visibleCount < sortedComment.length"
+        class="flex justify-center mt-6"
+      >
+        <BaseButton
+          variant="accent"
+          size="sm"
+          class="flex items-center gap-2 text-xs sm:text-base"
+          @click="loadMore"
+        >
+          <svg
+            class="w-4 h-4"
+            fill="none"
+            stroke="currentColor"
+            stroke-width="2"
+            viewBox="0 0 24 24"
+          >
+            <path
+              stroke-linecap="round"
+              stroke-linejoin="round"
+              d="M19 9l-7 7-7-7"
+            ></path>
+          </svg>
+          Show More Comments
+        </BaseButton>
+      </div>
+    </div>
     <!-- 0 comment -->
     <div
       v-if="visibleComments.length === 0"
-      class="mt-6 p-4 text-center border rounded-lg bg-surface-hover"
+      class="mx-auto mt-6 p-4 text-center w-1/2"
     >
-      <p class="text-sm">
+      <p class="text-sm sm:text-base font-display text-gray-400">
         No comments yet. Be the first to share your thoughts!
       </p>
-    </div>
-
-    <!-- Comments List -->
-    <div class="space-y-4">
-      <BaseCommentCard
-        v-for="comment in visibleComments"
-        :key="comment.id"
-        :comment="comment"
-      />
-    </div>
-
-    <!-- Show More -->
-    <div
-      v-if="visibleCount < sortedComment.length"
-      class="flex justify-center mt-6"
-    >
-      <BaseButton
-        variant="accent"
-        class="px-6 py-2 shadow-md flex items-center gap-2"
-        @click="loadMore"
-      >
-        <svg
-          class="w-4 h-4"
-          fill="none"
-          stroke="currentColor"
-          stroke-width="2"
-          viewBox="0 0 24 24"
-        >
-          <path
-            stroke-linecap="round"
-            stroke-linejoin="round"
-            d="M19 9l-7 7-7-7"
-          ></path>
-        </svg>
-        Show More
-      </BaseButton>
     </div>
   </div>
 </template>
@@ -89,12 +106,22 @@
 <script setup lang="ts">
 import { useUserData } from "~/store/user";
 import type { Comments } from "~/types/types";
-import BaseCommentCard from "./BaseCommentCard.vue";
+import BaseCommentCard from "./BaseComponents/BaseCommentCard.vue";
 import BaseButton from "../base/BaseButton.vue";
 
 const props = defineProps<{
   mediaId: number;
 }>();
+
+const {
+  data: comments,
+  status,
+  error,
+  refresh,
+} = useAsyncData(
+  () => $fetch<Comments[]>(`/api/MediaReview?mediaId=${props.mediaId}`),
+  { default: () => [] }
+);
 
 const userStore = useUserData();
 const isLoggedIn = computed(() => userStore.isLoggedIn);
@@ -102,7 +129,6 @@ const userId = computed(() => userStore.userInfo.id);
 
 const newComment = ref("");
 const isSubmitting = ref(false);
-const comments = ref<Comments[]>([]);
 
 const sortedComment = computed(() =>
   [...comments.value].sort((a, b) => {
@@ -116,17 +142,6 @@ const visibleComments = computed(() =>
 );
 const loadMore = () => {
   visibleCount.value += 3;
-};
-
-const fetchComments = async () => {
-  try {
-    const data = await $fetch<Comments[]>(
-      `/api/MediaReview?mediaId=${props.mediaId}`
-    );
-    comments.value = data || [];
-  } catch (error) {
-    console.error("Error loading comments", error);
-  }
 };
 
 const submitComment = async () => {
@@ -145,7 +160,7 @@ const submitComment = async () => {
 
     if ("success" in response && response.success) {
       newComment.value = "";
-      await fetchComments();
+      await refresh();
     } else {
       console.error((response as { error: string }).error);
     }
@@ -155,8 +170,4 @@ const submitComment = async () => {
     isSubmitting.value = false;
   }
 };
-
-onMounted(() => {
-  fetchComments();
-});
 </script>

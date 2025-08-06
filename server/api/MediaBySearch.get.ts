854a2cecfd54bf6d13ab793f004c471e75da8e2b
AliArefnia <maarefnia1377@gmail.com>
2025-08-06 18:27:08 +0330

    Update MediaBySearch.get.ts
    remove image url and width so handle by the UI components

--- server/api/MediaBySearch.get.ts
+++ server/api/MediaBySearch.get.ts
@@ -3,9 +3,7 @@ import type { MovieGet, SearchResult } from "~/types/types";
 export default defineEventHandler(async (event) => {
   try {
     const config = useRuntimeConfig();
-    const { width, searchTerm, page } = getQuery(event);
-
-    const IMAGE_URL = `https://image.tmdb.org/t/p/w${width}`;
+    const { searchTerm, page, genre, year, rating } = getQuery(event);
 
     const response = await $fetch<{ results: SearchResult[] }>(
       `https://api.themoviedb.org/3/search/multi?query=${searchTerm}&include_adult=false&language=en-US&page=${page}`,
@@ -18,7 +16,7 @@ export default defineEventHandler(async (event) => {
       }
     );
 
-    return response.results
+    let results = response.results
       .map((item: any): SearchResult | null => {
         if (item.media_type === "movie") {
           return {
@@ -26,9 +24,7 @@ export default defineEventHandler(async (event) => {
             media_type: "movie",
             title: item.title || "Untitled",
             vote_average: Number((item.vote_average ?? 0).toFixed(1)),
-            poster_path: item.poster_path
-              ? `${IMAGE_URL}${item.poster_path}`
-              : null,
+            poster_path: item.poster_path ?? null,
             release_date: (item.release_date || "N/A").slice(0, 4),
             genre_ids: item.genre_ids || [],
           };
@@ -38,9 +34,7 @@ export default defineEventHandler(async (event) => {
             media_type: "tv",
             name: item.name || "Untitled",
             vote_average: Number((item.vote_average ?? 0).toFixed(1)),
-            poster_path: item.poster_path
-              ? `${IMAGE_URL}${item.poster_path}`
-              : null,
+            poster_path: item.poster_path ?? null,
             first_air_date: (item.first_air_date || "N/A").slice(0, 4),
             genre_ids: item.genre_ids || [],
           };
@@ -49,17 +43,13 @@ export default defineEventHandler(async (event) => {
             id: item.id,
             media_type: "person",
             name: item.name,
-            profile_path: item.profile_path
-              ? `${IMAGE_URL}${item.profile_path}`
-              : null,
+            profile_path: item.profile_path ?? null,
             known_for: item.known_for.map((kf: any) => ({
               id: kf.id,
               title: kf.title,
               name: kf.name,
               media_type: kf.media_type,
-              poster_path: kf.poster_path
-                ? `${IMAGE_URL}${kf.poster_path}`
-                : null,
+              poster_path: kf.poster_path ?? null,
             })),
             known_for_department: item.known_for_department,
           };

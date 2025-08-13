23f5cb7a3b603b42768d7200920f9eaa4f6f7a36
AliArefnia <maarefnia1377@gmail.com>
2025-08-13 16:04:34 +0330

    change layout based on viewport width size

--- layouts/default.vue
+++ layouts/default.vue
@@ -4,11 +4,28 @@
     <main class="bg-surface-dark flex-grow">
       <slot></slot>
     </main>
-    <Footer />
+    <ClientOnly>
+      <Footer v-if="width < breakpoint" />
+    </ClientOnly>
   </div>
 </template>
 
 <script setup>
 import Header from "~/components/Layout/Header.vue";
 import Footer from "~/components/Layout/Footer.vue";
+
+const width = ref(1024);
+const breakpoint = 768;
+
+function updateWidth() {
+  width.value = window.innerWidth;
+}
+
+onMounted(() => {
+  updateWidth();
+  window.addEventListener("resize", updateWidth);
+});
+onUnmounted(() => {
+  window.removeEventListener("resize", updateWidth);
+});
 </script>
